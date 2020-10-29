fce9aebd3398744067acefca3bb6fa0953f55bc7
Prabhakar Rai <student343.bhu@gmail.com>
2020-10-29 22:26:31 +0530

    Update eduTypingScript.user.js

--- eduTypingScript.user.js
+++ eduTypingScript.user.js
@@ -1,10 +1,13 @@
 // ==UserScript==
 // @name         EduTyping Bot (Script)
 // @namespace    https://www.theprabhakar.in/
-// @version      1.0
+// @version      0.1
 // @description  Browser counterpart of EduTyping Bot
 // @author       Prabhakar Rai
 // @match        https://www.edutyping.com/student/lesson/*
+// @icon         https://github.com/PrabhakarRai/EduTyping-Auto-Bot/raw/main/icon.png
+// @updateURL    https://github.com/PrabhakarRai/EduTyping-Auto-Bot/raw/main/eduTypingScript.user.js
+// @downloadURL  https://github.com/PrabhakarRai/EduTyping-Auto-Bot/raw/main/eduTypingScript.user.js
 // @require      https://code.jquery.com/jquery-3.5.1.min.js
 // @grant        unsafeWindow
 // @grant        GM_addStyle
